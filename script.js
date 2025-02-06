cec06886f189d973e2a8be9c0b8888b099781ccd
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-06 09:20:41 -0800

    asd
    asd

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 console.log("started")
 
 function AwardButton() {
-    console.log("recived input")
+    console.log("recived input A")
     let change = document.getElementById("AwarderText").value;
-    document.getElementById("Give").innerHTML = change;
-    console.log(change)
+    if (change == "") {
+        document.getElementById("Give").innerHTML = "invalid";
+        console.log("that dont work")
+    } else {
+        document.getElementById("Give").innerHTML = change;
+        console.log(change)
+    }
 }
+
+function ReceiveButton() {
+    console.log("recived input R")
+    let change = document.getElementById("ReceiverText").value;
+    if (change == "") {
+        document.getElementById("Take").innerHTML = "invalid";
+        console.log("that dont work")
+    } else {
+        document.getElementById("Take").innerHTML = change;
+        console.log(change)
+    }
+}
+
+function SetTime() {
+    const date = Date();
+    console.log(date)
+    // const dayMonthYear =
+}
+
+setInterval(SetTime, 1000)
