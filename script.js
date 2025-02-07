a97c80cef40a9fdd2b176908d9535fd51e67c76b
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-07 09:06:55 -0800

    yea this seems like a finished product to me
    yeap

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function AwardButton() {
     console.log("recived input A")
     let change = document.getElementById("AwarderText").value;
     if (change == "") {
-        document.getElementById("Give").innerHTML = "invalid";
+        document.getElementById("Give").innerHTML = "Empty";
         console.log("that dont work")
     } else {
         document.getElementById("Give").innerHTML = change;
@@ -16,7 +16,7 @@ function ReceiveButton() {
     console.log("recived input R")
     let change = document.getElementById("ReceiverText").value;
     if (change == "") {
-        document.getElementById("Take").innerHTML = "invalid";
+        document.getElementById("Take").innerHTML = "Empty";
         console.log("that dont work")
     } else {
         document.getElementById("Take").innerHTML = change;
@@ -41,3 +41,11 @@ function SetTime() {
 
 setInterval(SetTime, 1000)
 
+function certificateChange(inputCertificate, left, middle, right) {
+    document.getElementById("leImportant").src = inputCertificate
+
+    document.getElementById("Give").style.transform = `translate(313px, ${left}px)`
+    document.getElementById("Time").style.transform = `translate(0px, ${middle}px)`
+    document.getElementById("Take").style.transform = `translate(-313px, ${right}px)`
+    console.log(left, middle, right)
+}
